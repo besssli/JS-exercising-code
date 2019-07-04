a982798c5af8f3e3503ccf6b11065bd3db67a83c
MacBook Pro <macbookpro@MacBookde-MacBook-Pro.local>
2019-07-04 15:40:23 +0800

    T9 Array methods - chaining methods

--- T9-Array Methods/sandbox.js
+++ T9-Array Methods/sandbox.js
@@ -1,37 +1,19 @@
-// example1 - sorting strings
-const names = ['mario','shaun', 'chun-li', 'yoshi', 'luigi'];
-
-// names.sort();
-// console.log(names);
-
-//example2 - sorting numbers
-const scores = [10, 50, 20, 5, 35, 70, 45];
-
-// scores.sort();
-// * result : [10, 50, 20, 5, 35, 70, 45] => compare the first letter
-// scores.reverse();
-scores.sort((a,b) => b - a);
-console.log(scores);
-
-// example3 - sorting objects
-
-const players = [
-    {name:'mario', score: 20},
-    {name: 'luigi', score: 10},
-    {name: 'chun-li', score: 50},
-    {name: 'yoshi', score: 30},
-    {name: 'shaun', score: 70}
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shells', price: 10},
+    {name: 'red shells', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50}
 ];
 
-// players.sort((a,b) => {
-//     if(a.score > b.score){
-//         return -1;
-//     }else if (b.score > a.score){
-//         return 1;
-//     }else{
-//         return 0;
-//     }
+// const filtered = products.filter(product => product.price > 20);
+
+// const promos = filtered.map(product => {
+//     return `the ${product.price} is ${product.price / 2} pounds`;
 // });
 
-players.sort((a,b) => b.score - a.score);
-console.log(players);
+
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the ${product.price} is ${product.price / 2} pounds`);
+console.log(promos);
