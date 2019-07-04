9f46b68f2e6c7946edc9bb362522a8380a130e46
MacBook Pro <macbookpro@MacBookde-MacBook-Pro.local>
2019-07-04 14:47:49 +0800

    T9 Array methods - find method

--- T9-Array Methods/sandbox.js
+++ T9-Array Methods/sandbox.js
@@ -1,28 +1,6 @@
-// reduce method
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+// find method
+const scores = [10, 5, 0, 40, 60, 10, 90, 70];
 
-// const result = scores.reduce((acc, curr) => {
-//     if(curr > 50){
-//         acc++;
-//     };
-//     return acc;
-// }, 0);
+const firstHighScore = scores.find( score =>  score > 50);
 
-// console.log(result);
-
-
-const scores = [
-    {name: 'mario', score: 50},
-    {name: 'tina', score: 70},
-    {name: 'mario' ,score: 60},
-    {name: 'jashun', score: 45}
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-    if(curr.name === 'mario'){
-        acc += curr.score
-    };
-    return acc;
-}, 0);
-
-console.log(marioTotal);
+console.log(firstHighScore);
