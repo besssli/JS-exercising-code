1b978b2e362e7dee0a680d7abac8079714a85c3f
MacBook Pro <macbookpro@MacBookde-MacBook-Pro.local>
2019-07-10 00:11:52 +0800

    T11 - using Dates-fns Library

--- T11-Dates and Times/sandbox.js
+++ T11-Dates and Times/sandbox.js
@@ -1,19 +1,16 @@
+const now = new Date();
 const clock = document.querySelector('.clock');
 
-const tick = () => {
-    const now = new Date();
+// console.log(dateFns.isToday(now));
 
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
 
-    const html = `
-        <span>${h}</span> : 
-        <span>${m}</span> : 
-        <span>${s}</span>
-    `;
+// formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMM YYYYY'));
 
-    clock.innerHTML = html;
-};
-
-setInterval(tick, 1000);
+// comparing dates
+const before = new Date('May 5 2019 04:40:00');
+console.log(dateFns.distanceInWords(now, before));
