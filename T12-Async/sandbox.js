c4d994c8115fa3dbbf2c9c27ea5ab534078eb9ca
besssli <leoisapig222@gmail.com>
2019-07-10 23:13:21 +0800

    T12 Async js - using async and await and throwing catching error

--- T12-Async/sandbox.js
+++ T12-Async/sandbox.js
@@ -1,11 +1,30 @@
-// fetch api
+// async & await
 
-fetch('todos/mario.json').then((response) => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch((err) => {
-    console.log('rejected', err);
-});
+const getTodos = async () => {
+    const response = await fetch('todos/sally.json');
+    if(response.status !== 200){
+        throw new Error('can not fetch the data');
+
+    }
+    const data = await response.json();
+    return data;
+
+};
+console.log(1);
+console.log(2);
+
+getTodos()
+    .then(data => {console.log('resolved:', data);})
+    .catch(err => console.log('rejected:', err.message));
+console.log(3);
+console.log(4);
+
+// fetch('todos/mario.json').then((response) => {
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log('rejected', err);
+// });
 
