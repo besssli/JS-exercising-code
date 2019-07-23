411dc555d043ea919309fbdedcf483f13e03d17e
besssli <leoisapig222@gmail.com>
2019-07-23 11:45:45 +0800

    T15 Object Oriented - prototype model and prototypal inheritance

--- T15 Object Oriented js/sandbox.js
+++ T15 Object Oriented js/sandbox.js
@@ -1,59 +1,32 @@
-// class User{
-//     constructor( userName, email){
-//         this.userName = userName;
-//         this.email = email;
-//         this.score = 0;
-//     }
-//     login(){
-//         console.log(`${this.userName} just logged in`)
-//         return this;
-//     }
-//     logout(){
-//         console.log(`${this.userName} just logged out`)
-//         return this;
-//     }
-//     incScore(){
-//         this.score += 1;
-//         console.log(`${this.userName} has a score of ${this.score}`);
-//         return this;
-//     }
-// }
-
-// class Admin extends User{
-//     constructor( userName, email, title){
-//         super( userName, email);
-//         this.title = title;
-//     }
-//     deleteUser(user){
-//         users = users.filter(u => u.userName !== user.userName);
-//     }
-// }
-
 // constructor fuction
 function User(userName, email){
     this.userName = userName;
     this.email = email;
-    this.login = function(){
-        console.log(`${this.userName} has logged in`)
-    };
 }
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it blinds a value of 'this' to the new empty object
-// 3 - it calls the constructor function to build the object
+User.prototype.login = function(){
+    console.log(`${this.userName} has logged in`)
+    return this
+};
+
+User.prototype.logout = function(){
+    console.log(`${this.userName} has logged out`)
+};
+
+function Admin(userName, email, title){
+    User.call(this, userName, email);
+    this.title = title;
+};
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(){
+    //delete User
+};
+
 const userOne = new User('mario', 'mario@giligruala.com');
-const userTwo = new User('justin', 'justin@giligruala.com')
-// const UserThree = new Admin('bess', 'bess.li@giligruala.com', 'CEO')
-console.log( userOne, userTwo);
-userOne.login();
+const userTwo = new User('justin', 'justin@giligruala.com');
+const userThree = new Admin('bess', 'bess@giligruala.com', 'CEO');
 
-// let users = [userOne, userTwo, UserThree];
-// console.log(users);
-
-// UserThree.deleteUser(userTwo);
-// console.log(users);
-
-// console.log(UserThree);
-
-
+console.log( userOne, userTwo, userThree);
+userOne.login().logout();
