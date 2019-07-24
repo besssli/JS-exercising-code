293346d973aa5a230498763efe932f15659260d5
besssli <leoisapig222@gmail.com>
2019-07-24 15:54:54 +0800

    T16 database - connecting firebase, and getting collections

---
+++ T16-Database/sandbox.js
@@ -0,0 +1,22 @@
+const list = document.querySelector('ul');
+
+const addRecipe = (recipe) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+    <li>
+      <div>${recipe.title} </div>
+      <div>${time} </div>
+    </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+db.collection('recipes').get().then((snapshot) => {
+  // when we have data
+  snapshot.docs.forEach(doc => {
+    addRecipe(doc.data());
+  });
+}).catch((err) => {
+  console.log(err);
+});
